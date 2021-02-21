c31f87d799a115208688b699ddd2c5e8c882a1f9
Shailesh <vspn8569@gmail.com>
2021-02-21 09:17:40 +0530

    Modifications added

--- js/script.js
+++ js/script.js
@@ -7,12 +7,38 @@ $(document).ready(function () {
 })
 
 const progressBar = document.querySelectorAll('.progress-bar');
+console.log(progressBar)
+const eduWork = document.querySelector('.eduWork-wrapper');
 
-progressBar.forEach(bar => {
-    const percent = bar.dataset.percent;
-    let tooltip = bar.children[0];
-    bar.style.width = `${percent}%`;
-    tooltip.innerText = `${percent}%`;
+
+
+
+window.addEventListener('scroll', () => {
+    const pos = eduWork.getBoundingClientRect().top;
+    const screenPos = window.innerHeight / 2;
+
+    // console.log(`Element pos: ${pos}`);
+    // console.log(`Screen Position: ${screenPos}`);
+
+    if (pos < screenPos) {
+        progressBar.forEach(bar => {
+            const percent = bar.dataset.percent;
+            let tooltip = bar.children[0];
+            bar.style.width = `${percent}%`;
+            setTimeout(function () {
+                tooltip.innerText = `${percent}%`;
+            }, 500)
+        })
+    } else {
+        progressBar.forEach(bar => {
+            bar.style.width = 0;
+            let tooltip = bar.children[0];
+            setTimeout(function () {
+                tooltip.innerText = ` `;
+            }, 500)
+        })
+    }
+
 })
 
 $(document).ready(function () {
@@ -23,4 +49,20 @@ $(document).ready(function () {
         prevArrow: '.next',
         nextArrow: '.prev'
     })
+})
+
+
+
+const backToTop = document.querySelector('.backToTop');
+
+window.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTop.classList.add('show');
+    } else {
+        backToTop.classList.remove('show');
+    }
+})
+
+backToTop.addEventListener('click', () => {
+    window.scrollTo(0, 0);
 })
